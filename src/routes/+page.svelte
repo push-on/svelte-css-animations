a7e3863ed3ff455c7b9cb4b141da7ec946fdbfb3
mhimranhossain <mh.imranhossain@outlook.com>
2023-07-25 21:17:53 +0600

    Update 07/25/2023 21:17:53

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div>
+  <h1>Home Page</h1>
+  <a href="/particles">css particles animation goin up</a>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
+
+// svelte.config.js
+
+import adapter from "@sveltejs/adapter-auto"
+import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
-};
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess({ scss: true }),
 
-export default config;
+	kit: {
+		adapter: adapter(),
+	},
+}
+
+export default config
